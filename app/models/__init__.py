37aca519de2dc9945ec7c74dade7d05c6f6405bf
Vinicius Damas <viniciusdamas97@gmail.com>
2026-06-02 10:03:02 -0300

    feat: adiciona tabelas de clientes e vendas, incluindo modelos e controladores para movimentações

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,10 @@
 from app.models import categoria
 from app.models import produto
 from app.models import usuario
+from app.models import movimentacao
+from app.models import cliente
+from app.models import venda
+
+# python -m alembic revision --autogenerate -m "criar tabelas clientes e vendas"
+
+# python -m alembic upgrade head
